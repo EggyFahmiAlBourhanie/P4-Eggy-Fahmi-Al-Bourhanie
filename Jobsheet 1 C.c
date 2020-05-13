a3af104f51da209942fadeae9f588990ee6fc94e
EggyFahmiAlBourhanie <64634533+EggyFahmiAlBourhanie@users.noreply.github.com>
2020-05-13 10:44:00 +0800

    Add files via upload
    Membuat array menggunakan bahasa C

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+main()
+{
+	int my_array[5] = { 1,23,17,4,-5};
+	printf("Data ke-1 = %d",my_array[0]);
+	printf("\nData ke-2 = %d",my_array[1]);
+	printf("\nData ke-3 = %d",my_array[2]);
+	printf("\nData ke-4 = %d",my_array[3]);
+	printf("\nData ke-5 = %d",my_array[4]);
+}
---
+++ Jobsheet 2 C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+main(){
+	int nim[5];
+	printf(" Nama: Eggy Fahmi Al Bourhanie\n");
+	printf(" NIM: F1B019045\n");
+	printf(" Kelompok: 9\n");
+	printf("Masukkan nilai ke-1=");
+	scanf("%i, &nim[0]");
+	printf("Masukkan nilai ke-2=");
+	scanf("%i, &nim[1]");
+	printf("Masukkan nilai ke-3=");
+	scanf("%i, &nim[2]");
+	printf("Masukkan nilai ke-4=");
+	scanf("%i, &nim[3]");
+	printf("Masukkan nilai ke-5=");
+	scanf("%i, &nim[4]");
+}
